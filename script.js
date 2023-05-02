4830cc1a4aa942a1f7f7f6f4370fcd9e8eee6a71
Jaber Amin <76704378+jaberamin9@users.noreply.github.com>
2023-05-02 17:28:03 +0600

    v0.4

--- script.js
+++ script.js
@@ -10,6 +10,13 @@ document.getElementById("menu-btn").onclick = () => {
     cnt++;
 };
 
+window.onclick = function (element) {
+    if (element.target == document.getElementById("full-container")) {
+        document.getElementById("full-content").innerHTML = "";
+        document.getElementById("full-container").style.visibility = "hidden";
+    }
+};
+
 
 fetch("https://openapi.programming-hero.com/api/news/categories")
     .then(i => i.json())
@@ -146,11 +153,11 @@ const singelContent = (data, name) => {
                     <div class="author">
                         <img src="${i.author.img}">
                         <div class="txt-author">
-                            <h4>${i.author.name}</h4>
+                            <h4>${i.author.name ? i.author.name : "Unknown User"}</h4>
                             <p>${i.author.published_date}</p>
                         </div>
                     </div>
-                    <i class="fa-regular fa-eye"> <span id="view"> ${i.total_view}</span></i>
+                    <i class="fa-regular fa-eye"> <span id="view"> ${i.total_view ? i.total_view : "No View"}</span></i>
                     <div class="ratting" id="ratting-${i._id}">
                         <i class="fa-solid fa-star"></i>
                         <i class="fa-solid fa-star"></i>
@@ -158,92 +165,136 @@ const singelContent = (data, name) => {
                         <i class="fa-regular fa-star"></i>
                         <i class="fa-regular fa-star"></i>
                     </div>
-                    <i class="fa-solid fa-arrow-right" id="go-btn"></i>
+                    <i class="fa-solid fa-arrow-right" id="go-btn" onclick="getNewsDetail('${i._id}')"></i>
                 </div>
             </div>
         </div>
     </div>`
-
-        let rat = document.getElementById("ratting-" + i._id);
-        if (i.rating.number == 0.0) {
-            rat.innerHTML = `
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>`
-        } else if (i.rating.number >= 0.1 && i.rating.number <= 0.5) {
-            rat.innerHTML = `
-        <i class="fa-solid fa-star-half-stroke"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>`
-        } else if (i.rating.number >= 0.6 && i.rating.number <= 1.0) {
-            rat.innerHTML = `
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>`
-        } else if (i.rating.number >= 1.1 && i.rating.number <= 1.5) {
-            rat.innerHTML = `
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star-half-stroke"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>`
-        } else if (i.rating.number >= 1.6 && i.rating.number <= 2.0) {
-            rat.innerHTML = `
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>`
-        } else if (i.rating.number >= 2.1 && i.rating.number <= 2.5) {
-            rat.innerHTML = `
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star-half-stroke"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>`
-        } else if (i.rating.number >= 2.6 && i.rating.number <= 3.0) {
-            rat.innerHTML = `
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star"></i>
-        <i class="fa-regular fa-star"></i>`
-        } else if (i.rating.number >= 3.1 && i.rating.number <= 3.5) {
-            rat.innerHTML = `
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star-half-stroke"></i>
-        <i class="fa-regular fa-star"></i>`
-        } else if (i.rating.number >= 3.6 && i.rating.number <= 4.0) {
-            rat.innerHTML = `
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-regular fa-star"></i>`
-        } else if (i.rating.number >= 4.1 && i.rating.number <= 4.5) {
-            rat.innerHTML = `
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star-half-stroke"></i>`
-        } else if (i.rating.number >= 4.6 && i.rating.number <= 5.0) {
-            rat.innerHTML = `
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>
-        <i class="fa-solid fa-star"></i>`
-        }
+        Ratting(i._id, i.rating.number);
     })
+}
+
+const getNewsDetail = (news_id) => {
+    let url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => showNewsDetail(data.data[0]));
+};
+
+const showNewsDetail = (newsDetail) => {
+    document.getElementById("full-container").style.visibility = "visible";
+    document.getElementById("full-content").innerHTML = `
+    <img src="${newsDetail.image_url}" style="width: 100%">
+                <h1>${newsDetail.title} <span id="f-is-trending">Trending</span> <span id="f-is-todays-pick">Todays Pick</span></h1>
+                <textarea rows="8">${newsDetail.details}
+                </textarea>
+                <div class="bottom-content" style="width: 100%;margin-top: 20px">
+                    <div class="author">
+                        <img src="${newsDetail.author.img}">
+                        <div class="txt-author">
+                        <h4>${newsDetail.author.name ? newsDetail.author.name : "Unknown User"}</h4>
+                        <p style="line-height: 0rem;margin-top: 13px;">${newsDetail.author.published_date}</p>
+                        </div>
+                    </div>
+                    <i class="fa-regular fa-eye"> <span id="view"> ${newsDetail.total_view ? newsDetail.total_view : "No View"}</span></i>
+                    <div class="ratting" id="ratting-${newsDetail._id}">
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star-half-stroke"></i>
+                        <i class="fa-regular fa-star"></i>
+                        <i class="fa-regular fa-star"></i>
+                    </div>
+                </div>
+    `;
+    if (newsDetail.others_info.is_trending) {
+        document.getElementById("f-is-trending").style.display = "inline";
+    }
+    if (newsDetail.others_info.is_todays_pick) {
+        document.getElementById("f-is-todays-pick").style.display = "inline";
+    }
+    Ratting(newsDetail._id, newsDetail.rating.number);
+};
+
+const Ratting = (id, number) => {
+    let rat = document.getElementById("ratting-" + id);
+    if (number == 0.0) {
+        rat.innerHTML = `
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>`
+    } else if (number >= 0.1 && number <= 0.5) {
+        rat.innerHTML = `
+    <i class="fa-solid fa-star-half-stroke"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>`
+    } else if (number >= 0.6 && number <= 1.0) {
+        rat.innerHTML = `
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>`
+    } else if (number >= 1.1 && number <= 1.5) {
+        rat.innerHTML = `
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star-half-stroke"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>`
+    } else if (number >= 1.6 && number <= 2.0) {
+        rat.innerHTML = `
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>`
+    } else if (number >= 2.1 && number <= 2.5) {
+        rat.innerHTML = `
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star-half-stroke"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>`
+    } else if (number >= 2.6 && number <= 3.0) {
+        rat.innerHTML = `
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-regular fa-star"></i>
+    <i class="fa-regular fa-star"></i>`
+    } else if (number >= 3.1 && number <= 3.5) {
+        rat.innerHTML = `
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star-half-stroke"></i>
+    <i class="fa-regular fa-star"></i>`
+    } else if (number >= 3.6 && number <= 4.0) {
+        rat.innerHTML = `
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-regular fa-star"></i>`
+    } else if (number >= 4.1 && number <= 4.5) {
+        rat.innerHTML = `
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star-half-stroke"></i>`
+    } else if (number >= 4.6 && number <= 5.0) {
+        rat.innerHTML = `
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>
+    <i class="fa-solid fa-star"></i>`
+    }
 }
 
 
